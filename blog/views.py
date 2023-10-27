f2d4c2bdb150937e1ee75b80341f01f09146f081
Pablo Céspedes <pablocesar.cespedes98@gmail.com>
2023-10-27 16:01:24 -0600

    Commit 2

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,get_object_or_404
+from .models import post
 # Create your views here.
 def render_post(request):
-    return render(request,'post.html')
+    posts= post.objects.all()
+    return render(request,'post.html',{'posts':posts})
+
+def post_detail(request, post_id):
+    posts = get_object_or_404(post, pk=post_id)
+    return render(request, 'post_detail.html', {'post':posts})
